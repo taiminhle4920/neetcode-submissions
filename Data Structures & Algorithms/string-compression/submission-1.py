70109374d7f24de3afcf603e57ed504dfc86e2df
Tai Le <66931054+taiminhle4920@users.noreply.github.com>
2026-07-22 09:40:07 -0700

    Add: string-compression - submission-1

---
+++ Data Structures & Algorithms/string-compression/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        if not chars:
+            return 0
+        res = 0
+        cur = 1
+        c = chars[0]
+
+
+        for i in range(1, len(chars)):
+            if chars[i] != c:
+                chars[res] = c
+                res += 1
+                if cur > 1:
+                    s = str(cur)
+                    for v in s:
+                        chars[res] = v
+                        res += 1
+                cur = 1
+                c = chars[i]
+            else:
+                cur += 1
+        
+        chars[res] = c
+        res += 1
+
+        if cur > 1:
+            s = str(cur)
+            for v in s:
+                chars[res] = v
+                res += 1
+
+        return res
